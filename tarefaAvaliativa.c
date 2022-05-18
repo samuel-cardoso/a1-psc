00fe01f8a3135d03c18a2c2c1383bdeed9e4ce13
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-05-17 21:35:31 -0300

    a1

--- tarefaAvaliativa.c
+++ tarefaAvaliativa.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main ()
-{ 
+
+float mediaPonderada(n1,n2){
+
+    n1;
+    n2;
+    
 
 
 }
+
+void main()
+{
+
+    int n1, n2;
+
+
+    printf("\n ===== Tarefa Avaliativa =====\n\n");
+    printf(" Digite um numero: ");
+    scanf("%d", &n1);
+    printf(" Digite outro numero: ");
+    scanf("%d", &n2);
+
+   /* if (n1 > 0 && n2 < 0 || n1 < 0 && n2 > 0 || n1 > 0 && n2 > 0 || n1 < 0 && n2 < 0)
+    {
+        if (n1 > 0 && n2 > 0)
+        {
+            printf("\n Temos dois numeros positivos: %d, %d", n1, n2);
+        }
+        else if (n1 < 0 && n2 < 0)
+        {
+            printf("\n Temos dois numeros negativos: %d, %d", n1, n2);
+        }
+        if (n1 > 0 && n2 < 0)
+        {
+            printf("\n Temos um numero positivo %d e um numero negativo %d ", n1, n2);
+        }
+        else if (n1 < 0 && n2 > 0)
+        {
+            printf("\n Temos um numero negativo %d e um numero positivo %d ", n1, n2);
+        }
+    } else {
+        printf("\n\n   DADO INVALIDO!!!\n\n");
+        printf(" Digite apenas numeros\n");
+    }
+
+   */
+
+mediaPonderada(n1,n2);
+
+} // fim da função main
