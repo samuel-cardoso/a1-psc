3b0ec6b8169c374847ed7edec9566d9d99417eae
SamuelCdaSilva <91702874+SamuelCdaSilva@users.noreply.github.com>
2022-05-20 01:45:51 -0300

    a1

--- tarefaAvaliativa.c
+++ tarefaAvaliativa.c
@@ -16,7 +16,7 @@ int tabuadas(valor1, valor2)
     int res1[10]; 
     int res2[10];
 
-    // Construção da primeira tabuada e a soma de seus resultados.
+    // Construção da primeira tabuada.
     printf("\n == Tabuada do %d ==\n\n", valor1);
     for (int numeros = 1; numeros <= 10; numeros++)
     {
@@ -24,9 +24,8 @@ int tabuadas(valor1, valor2)
     }        
 
     int somaTab1 = res1[1]+res1[2]+res1[3]+res1[4]+res1[5]+res1[6]+res1[7]+res1[8]+res1[9]+res1[10];
-    printf("\n A soma de todos os resultados: %d\n", somaTab1); 
 
-    // Construção da segunda tabuada e a soma de seus resultados.
+    // Construção da segunda tabuada.
     printf("\n == Tabuada do %d ==\n\n", valor2);
     for (int numeros = 1; numeros <= 10; numeros++)
     {   
@@ -34,12 +33,11 @@ int tabuadas(valor1, valor2)
     }
     
     int somaTab2 = res2[1]+res2[2]+res2[3]+res2[4]+res2[5]+res2[6]+res2[7]+res2[8]+res2[9]+res2[10];
-    printf("\n A soma de todos os resultados: %d\n", somaTab2);
 
     // Construção da soma dos resultados das duas tabuadas e aplicando a média ponderada e a retornando.
     int somaTab1e2 = somaTab1 + somaTab2;
-    float mediaPond = ((valor1 * 1) + (valor2 * 3) + (somaTab1e2 * 5)) / (1 + 3 + 5);
-    return mediaPond; 
+    printf("\n\n A soma de todos os resultados das duas tabuadas: %d",somaTab1e2);
+    return somaTab1e2; 
 }
 
 
@@ -64,7 +62,7 @@ void main()
             printf("\n Temos dois numeros negativos: %d, %d", valor1, valor2);
         }
         else if (valor1 > 0 && valor2 < 0)
-        {
+        {   
             printf("\n Temos um numero positivo %d e um numero negativo %d ", valor1, valor2);
         }
         else if (valor1 < 0 && valor2 > 0)
@@ -72,11 +70,12 @@ void main()
             printf("\n Temos um numero negativo %d e um numero positivo %d ", valor1, valor2);
         } 
 
-        //Chamando as funções.
+        //Chamando as funções, realizando a média ponderada e imprimindo na tela os resultados.
     float mediaFinal = mediaPonderada(valor1, valor2);
     printf("\n\n Media Final = %.1f\n", mediaFinal);
-    
-    int mediaPondTabs = tabuadas(valor1, valor2);
-    printf("\n\n Media ponderada: %d\n\n",mediaPondTabs);
+
+    int somaTab1e2 = tabuadas(valor1, valor2);
+    int mediaPond = ((valor1 * 1) + (valor2 * 3) + (somaTab1e2 * 5)) / (1 + 3 + 5);
+    printf("\n\n Media ponderada de %d, %d e %d: %d\n\n", valor1, valor2, somaTab1e2, mediaPond);
 
 } 
